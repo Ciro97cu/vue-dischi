36f3f1e0bad9df3b76313b40ebb11dc896ea9cbc
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-08 14:52:18 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HeaderSpotify />
+    <MainSpotify />
+  </div>
+</template>
+
+<script>
+import HeaderSpotify from "./components/HeaderSpotify.vue";
+import MainSpotify from "./components/MainSpotify.vue";
+
+// import axios from 'axios';
+
+export default {
+  name: "App",
+  components: {
+    HeaderSpotify,
+    MainSpotify,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/style/imports";
+#app {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
---
+++ src/components/HeaderSpotify.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "HeaderSpotify",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
